1a08d831c339ce2f1b0f88137dc778d46bfa8da7
HrishiDarade <daradeh2002@gmail.com>
2025-06-25 19:15:19 +0530

    updated argument script

--- arguments.sh
+++ arguments.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 echo "if you want to print the argument passed after the file we used the '\$@' "
-
 echo "Arguments: $@"
+
+echo " "
+
+echo "if you wnat the count how many arguments were passed then we use '\$#' "
+echo "No. of Arguments:  $# "
+
+echo " "
+
+echo "$*"
